0e0259cba6fe51e76e46d4dc532fc75a2e855cd5
janakiram <vallapujanakiram@gmail.com>
2025-08-17 18:24:27 +0530

    using ladder if

--- largestofthree.c
+++ largestofthree.c
@@ -4,6 +4,21 @@ int main()
     int num1,num2,num3;
     printf("enter three numbers:");
     scanf("%d %d %d",&num1,&num2,&num3);
-    (num1>num2&&num1>num3)?printf("%d",num1):(num2>num1&&num2>num3)?printf("%d",num2):(num1==num2&&num2==num3)?printf("all are equal"):printf("%d",num3);
+    if(num1>num2&&num1>num3)
+    {
+    printf("%d",num1);
+    }
+    else if(num2>num1&&num2>num3)
+    {
+        printf("%d",num2);
+    }
+    else if(num1==num2&&num2==num3)
+    {
+        printf("all are equal");
+    }
+    else
+    {
+    printf("%d",num3);
+    }
     return 0;
 }
