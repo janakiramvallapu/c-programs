fd7cf50cb7f1701c65db12a4822d50ee4b0e0f2c
janakiram <vallapujanakiram@gmail.com>
2025-08-17 19:25:01 +0530

    using logical operators

---
+++ Leap_year_check.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int year;
+    scanf("%d",&year);
+    ((year%4==0)&&(year%100!=0)||(year%400==0))&&printf("leap year")||printf("not a leap year");
+    return 0;
+}
