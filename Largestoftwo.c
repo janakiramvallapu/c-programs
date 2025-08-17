35bdef83b0f968b57ec20a9a1eccbe5bdb93a8f3
janakiram <vallapujanakiram@gmail.com>
2025-08-17 15:11:00 +0530

    using simple if

--- Largestoftwo.c
+++ Largestoftwo.c
@@ -4,7 +4,10 @@ int main()
     int num1,num2;
     printf("Enter two numbers:");
     scanf("%d %d",&num1,&num2);
-    (num1>num2)?printf("%d",num1):printf("%d",num2);
+    if(num1>num2)
+    printf("%d",num1);
+    if(num1<num2)
+    printf("%d",num2);
     return 0;
     
 }
