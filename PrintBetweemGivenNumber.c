2d3082371fd56c2e3dbce89361eef1d084c18fc8
janakiram <vallapujanakiram@gmail.com>
2025-08-20 21:42:35 +0530

    using while loop

---
+++ PrintBetweemGivenNumber.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2;
+    scanf("%d %d",&num1,&num2);
+    if(num1==num2)
+    {
+        printf("%d",num1);
+        return 0;
+    }
+    if(num1>num2)
+    {
+        printf("Invalid input. N1 should be less than or equal to N2.");
+        return 0;
+    }
+        
+        while(num1<=num2)
+        {
+            printf("%d",num1);
+            if(num1<num2)
+            {
+                printf(",");
+            }
+            num1++;
+        }
+    
+    return 0;
+}
