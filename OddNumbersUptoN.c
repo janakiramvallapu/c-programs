299e0202297a9508984ccb1087f8c03c66cdee9d
janakiram <vallapujanakiram@gmail.com>
2025-08-20 21:40:38 +0530

    using while loop

--- OddNumbersUptoN.c
+++ OddNumbersUptoN.c
@@ -1,25 +1,22 @@
-#include <stdio.h>
-
+#include<stdio.h>
 int main()
 {
     int num,count=0;
     scanf("%d",&num);
     if(num<0)
     {
-        printf("invalid");
+        printf("Invalid input. Program terminated.");
         return 0;
     }
-    label:
-    count++;
-    if(count%2!=0)
+    printf("Odd numbers up to %d: ",num);
+    while(count<num)
     {
-    printf("%d ",count);
+        count++;
+        if(count%2!=0)
+        {
+             printf("%d ",count);
+        }
     }
-    if(count<num)
-    {
-        goto label;
-    }
-    
-
     return 0;
+
 }
