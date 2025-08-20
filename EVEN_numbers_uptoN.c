b1e7cf03353146fea98d7d64c40c3e531766576c
janakiram <vallapujanakiram@gmail.com>
2025-08-20 21:39:13 +0530

    using while loop

--- EVEN_numbers_uptoN.c
+++ EVEN_numbers_uptoN.c
@@ -1,25 +1,21 @@
-#include <stdio.h>
-
+#include<stdio.h>
 int main()
 {
     int num,count=1;
     scanf("%d",&num);
     if(num<0)
     {
-        printf("invalid");
+        printf("Invalid input.");
         return 0;
     }
-    label:
-    count++;
-    if(count%2==0)
+    printf("Even numbers up to %d are: ",num);
+    while(count<num)
     {
-    printf("%d ",count);
+        count++;
+        if(count%2==0)
+        {
+            printf("%d ",count);
+        }
     }
-    if(count<num)
-    {
-        goto label;
-    }
-    
-
     return 0;
 }
